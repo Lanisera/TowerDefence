23fecfadeefa1f33df56a7a8d65c07165c777c28
laniser <laniser0905@gmail.com>
2024-11-10 16:09:36 +0800

    Separate definition and implementation

--- include/manager/game_manager.h
+++ include/manager/game_manager.h
@@ -20,68 +20,13 @@ class GameManager: public Manager<GameManager>
 	friend class Manager<GameManager>;
 	
 public:
-	int run(int argc, char **argv)
-	{
-		Uint64 last_counter = SDL_GetPerformanceCounter();
-		Uint64 counter_freq = SDL_GetPerformanceFrequency();
-
-		while (!is_quit)
-		{
-			// 处理事件
-			while (SDL_PollEvent(&event))
-				on_input();
-
-			Uint64 cur_counter = SDL_GetPerformanceCounter();
-			double delta = (double)(cur_counter - last_counter) / counter_freq;
-			last_counter = cur_counter;
-			if (delta * 1000.0 < 1000.0 / 60)
-					SDL_Delay((Uint32)((1000.0 / 60) - delta * 1000.0));
-
-			// 更新数据
-
-			on_update(delta);
-
-			// 绘制画面
-			SDL_SetRenderDrawColor(render, 0, 0, 0, 255);
-			SDL_RenderClear(render);
-
-			on_render();
-		}
-
-		return 0;
-	}
+	int run(int argc, char **argv);
 
 protected:
 	
-	GameManager()
-	{
-		init_assert(!SDL_Init(SDL_INIT_EVERYTHING), u8"SDL2 初始化失败");
-		init_assert(IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG), u8"SDL_image 初始化失败");
-		init_assert(Mix_Init(MIX_INIT_MP3), u8"SDL_mixer 初始化失败");
-		init_assert(!TTF_Init(), u8"SLD_ttf 初始化失败");
-		
-		Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+	GameManager();
 
-		SDL_SetHint(SDL_HINT_IME_SHOW_UI, "-1");
-
-		window = SDL_CreateWindow(u8"TowerDefence", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720,  SDL_WINDOW_SHOWN);
-		init_assert(window, u8"创建窗口失败");
-
-		render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE);
-		init_assert(render, u8"渲染器创建失败");
-	}
-
-	~GameManager()
-	{
-		SDL_DestroyRenderer(render);
-		SDL_DestroyWindow(window);
-
-		TTF_Quit();
-		Mix_Quit();
-		IMG_Quit();
-		SDL_Quit();
-
-	}
+	~GameManager();
 
 private:
 	SDL_Event event;
@@ -92,30 +37,11 @@ private:
 	SDL_Renderer *render = nullptr; 
 
 private:
-	void init_assert(bool flag, const char* err_msg)
-	{
-		if (flag) return ;
-		
-		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, u8"游戏启动失败", err_msg, window);
-		exit(-1);
-	}
+	void init_assert(bool flag, const char* err_msg);
 
-	void on_input()
-	{
-		if (event.type == SDL_QUIT)
-		{
-			is_quit = true;
-		}
+	void on_input();
 
-	}
+	void on_update(double delta);
 
-	void on_update(double delta)
-	{
-
-	}
-
-	void on_render()
-	{
-
-	}
+	void on_render();
 };
--- include/map/route.h
+++ include/map/route.h
@@ -13,58 +13,12 @@ public:
 
 public:
 	
-	Route(const TileMap& map, const SDL_Point& idx_origin)
-	{
-		size_t map_width = map[0].size();
-		size_t map_height = map.size();
-		SDL_Point idx_next = idx_origin;
-
-		while (true)
-		{
-			if (idx_next.x >= map_width || idx_next.y >= map_height)
-				break;
-
-			if (check_duplicate_point(idx_next))
-				break;
-			else
-				idx_list.push_back(idx_next);
-
-			bool is_next_dir_exist = true;
-			const Tile& tile = map[idx_next.y][idx_next.x];
-
-			// update next point
-			switch (tile.direction) 
-			{
-			case Tile::Direction::Up:
-				idx_next.y--;
-				break;
-			case Tile::Direction::Down:
-				idx_next.y++;
-				break;
-			case Tile::Direction::Left:
-				idx_next.x--;
-				break;
-			case Tile::Direction::Right:
-				idx_next.x++;
-				break;
-			default:
-				is_next_dir_exist = false;
-				break;
-			}
-
-			if (!is_next_dir_exist)
-				break;
-
-		}
-	}
+	Route(const TileMap& map, const SDL_Point& idx_origin);
 
 	Route() = default;
 	~Route() = default;
 
-	const IdxList& get_route() const
-	{
-		return idx_list;
-	}
+	const IdxList& get_route() const;
 
 protected:
 
@@ -73,14 +27,6 @@ private:
 	IdxList idx_list;
 
 private:
-	bool check_duplicate_point(const SDL_Point& target)
-	{
-		for (const auto& point : idx_list)
-			if (point.x == target.x && point.y == target.y)
-				return false;
-
-		return true;
-	}
-	
+	bool check_duplicate_point(const SDL_Point& target);
 
 };
