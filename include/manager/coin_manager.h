f09028ddef311919e5ee77c8a07e529f7e2af1b0
laniser <laniser0905@gmail.com>
2024-11-21 10:23:28 +0800

    add more function in CoinManager class

--- include/manager/coin_manager.h
+++ include/manager/coin_manager.h
@@ -1,21 +1,41 @@
 #pragma once
 
 #include "manager/manager.h"
+#include "prop/coin_prop.h"
+#include <SDL_render.h>
+#include <vector>
 
 class CoinManager : public Manager<CoinManager>
 {
 	friend class Manager<CoinManager>;
+public:
+	using CoinPropList = std::vector<CoinProp*>;
 
 public:
 	void increase_coin(double val);
 
 	void decrease_coin(double val);
 
+	void on_update(double delta);
+
+	void on_render(SDL_Renderer* renderer);
+
+	void spawn_coin_prop(const Vector2& position);
+		
+	CoinPropList& get_coin_prop_list();
+
+	double get_current_coin_num();
+	
 protected:
 	CoinManager();
 
-	~CoinManager() = default;
+	~CoinManager();
 
 private:
 	double num_coin = 0;
+
+	CoinPropList coin_prop_list;
+
+private:
+	void remove_invalid_coin_prop();
 };
--- src/coin_manager.cpp
+++ src/coin_manager.cpp
@@ -1,10 +1,8 @@
 #include "manager/coin_manager.h"
 #include "manager/config_manager.h"
-
-CoinManager::CoinManager()
-{
-	num_coin = ConfigManager::instance()->num_initial_coin;
-}
+#include "prop/coin_prop.h"
+#include "vector/vector2.h"
+#include <algorithm>
 
 void CoinManager::increase_coin(double val)
 {
@@ -19,4 +17,59 @@ void CoinManager::decrease_coin(double val)
 	{
 		num_coin = 0;
 	}
+}
+
+void CoinManager::on_update(double delta)
+{
+	for (auto coin_prop : coin_prop_list)
+		coin_prop->on_update(delta);
+		
+	remove_invalid_coin_prop();
+}
+
+void CoinManager::on_render(SDL_Renderer* renderer)
+{
+	for (auto coin_prop : coin_prop_list)
+		coin_prop->on_render(renderer);
+}
+
+void CoinManager::spawn_coin_prop(const Vector2& position)
+{
+	CoinProp *coin_prop = new CoinProp();
+	coin_prop->set_position(position);
+
+	coin_prop_list.push_back(coin_prop);
+}
+
+CoinManager::CoinPropList& CoinManager::get_coin_prop_list()
+{
+	return coin_prop_list;
+}
+
+double CoinManager::get_current_coin_num()
+{
+	return num_coin;
+}
+
+CoinManager::CoinManager()
+{
+	num_coin = ConfigManager::instance()->num_initial_coin;
+}
+
+CoinManager::~CoinManager()
+{
+	for (auto coin_prop : coin_prop_list)
+		delete  coin_prop;
+}
+
+void CoinManager::remove_invalid_coin_prop()
+{
+	coin_prop_list.erase(std::remove_if(coin_prop_list.begin(), coin_prop_list.end(), 
+				[&](const CoinProp* coin_prop)
+				{
+					bool deletebale = coin_prop->can_remove();
+					if (deletebale)
+						delete coin_prop;
+					return deletebale;
+				}), coin_prop_list.end());
 }
